836f186d3f4b9ca18e789f0269c83643ff8f9821
M <michael.willy@gmail.com>
2021-04-30 03:02:48 -0400

    figure 2+4 plot reproduction
    spacial kernel gaussian derivative
    neural interaction bifircation diagram

---
+++ Python/spatialKernel/spatial_kernel.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lib.plotformat import setup
+
+
+def gaussian(x: np.ndarray,
+             a: float,
+             c: float,
+             b: float = 0):
+    return a*np.exp(-(x-b)**2/2/c**2)
+
+def wavelet_dist(x: float,
+                 a: float,
+                 c: float,
+                 b: float = 0):
+    return (3*a*np.exp(-(-b + x)**2/(2*c**2))/c**4
+    - a*(-8*b + 8*x)*(-2*b + 2*x)*np.exp(-(-b + x)**2/(2*c**2))/(8*c**6)
+    - a*(-2*b + 2*x)**2*np.exp(-(-b + x)**2/(2*c**2))/c**6
+    + a*(-2*b + 2*x)**4*np.exp(-(-b + x)**2/(2*c**2))/(16*c**8)
+    )
+
+
+def plot_wavelet(X, plot_title = 'placeholder'):
+    fmt = setup(plot_title)  # plotting format obj
+    fig = plt.figure(figsize=(9,9))
+    ax = fig.add_subplot(111)
+    ax.plot(X[...,0],X[...,1],'-b')
+
+    plt.axis('tight')
+    # ax.legend(loc=3)
+    plt.title(plot_title)
+    plt.grid(b=True, which='major', axis='both')
+    plt.show()
+    fig.savefig(fmt.plot_name(plot_title,'png'))
+
+
+def main():
+    dist = 40
+    a = 10000/3*2
+    c = 10
+    # b = 0 mean
+    x = np.linspace(-dist,dist,1000)
+    g = gaussian(x,a,c,0)
+    w = wavelet_dist(x,a,c,0)
+
+    plot_wavelet(np.asarray([x,g]).T,'Gaussian')
+    plot_wavelet(np.asarray([x,w]).T,'Fourth Derivative Gaussian')
+
+
+if __name__ == '__main__':
+    main()
