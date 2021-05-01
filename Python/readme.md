29285ca337e858265348c5029e6fe2e91c6c1428
M <michael.willy@gmail.com>
2021-05-01 04:06:23 -0400

    arbitrary peak wavelet + started 2d plot
    cleaned up symbolics so can create 4th, nth derivative wavelet spatial kernal
    cast ic array for cortical sheet

--- Python/readme.md
+++ Python/readme.md
@@ -1,1 +1,26 @@
-placeholder
+#spatialKernel
+spatial_kernel.py<br>
+Figure 2<br>
+construct wavelet for distance decay spatial kernel<br>
+returns a normalized gaussian nth order derivative<br>
+
+
+scale 'a' nonlinearly ~10^n for magnitude<br>
+'b' is center mass<br>
+scale 'c' linearly for width<br>
+'d' is order of derivative for arbitrary spatial_wavelet <br>
+  params = {'a': 10000/3*2,
+            'b': 0,
+            'c': 10,
+            'order': 19,
+            }
+  w = kernel(spatial_wavelet,x,*params.values(),True)
+
+symbolic_calcs.py<br>
+recursive sympy differentiation for gaussian<br>
+
+#secondOrderInteraction
+neuralInteraction.py<br>
+Eq 13 Figure 4<br>
+second order neural interrupting interactions<br>
+returns a two term phase differential equation<br>
--- Python/secondOrderInteraction/neuralInteraction.py
+++ Python/secondOrderInteraction/neuralInteraction.py
@@ -1,8 +1,11 @@
+"""
+second order neural interrupting interactions
+"""
+
+
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import os
-import sys
 
 
 # custom scripts
@@ -13,20 +16,29 @@ from lib.plotformat import setup
 """
 def gamma(x: np.ndarray,
           beta: float = 1/4,
-          r: float = 8/10):
+          r: float = 8/10
+          ) -> np.ndarray:
     return -np.sin(x+beta) + r*np.sin(2*x)
 
 
 
-def plot_phase(X, plot_title = 'placeholder'):
+def plot_phase(X: np.ndarray,
+               plot_title:str = 'placeholder',
+               y_axis:str = 'y',
+               x_axis:str = 'x',
+               ):
     fmt = setup(plot_title)  # plotting format obj
     fig = plt.figure(figsize=(9,9))
     ax = fig.add_subplot(111)
     ax.plot(X[...,0]/np.pi,X[...,1],'-b')
-    ax.plot(np.asarray([X[0,0],X[-1,0]])/np.pi,[0,0],'-k')  #
-    plt.axis('tight')
+    ax.plot(np.asarray([X[0,0],X[-1,0]])/np.pi,[0,0],'-k',linewidth=1)  #
+
+    # plt.autoscale(enable=True, axis='both', tight=True)
+    # plt.axis('tight')
     # ax.legend(loc=3)
     plt.title(plot_title)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
 
     ax.xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%g $\pi$'))
     ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(base=1))
@@ -36,19 +48,21 @@ def plot_phase(X, plot_title = 'placeholder'):
 
 
 def main():
-    dist = np.pi
-
+    """"""
     params = [
               {'beta': 0.25, 'r':0.95},
               {'beta': 0.25, 'r':0.8},
               {'beta': 0.25, 'r':0.7},
               ]
-    i =2
-    x = np.linspace(-dist,dist,1000)
-    g = gamma(x,**params[i])
+    x = np.linspace(-np.pi,np.pi,1000)
 
-    r = params[i]['r']
-    plot_phase(np.asarray([x,g]).T,f'R = {r}')
+    for p in params:
+        g = gamma(x,**p)
+        plot_phase(np.asarray([x,g]).T,
+                   'R = {0}'.format(p['r']),
+                   '',
+                   # r'$\frac{d\theta}{dt}$',
+                   r'$\phi$')
 
 
 if __name__ == '__main__':
