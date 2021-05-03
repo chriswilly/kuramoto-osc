e97667cc5a78814688d38fb35cac49872183af22
M <michael.willy@gmail.com>
2021-05-02 20:43:27 -0400

    fixed phase plot scaling to 0 to pi
    added inferred titling based on %pi

--- Python/kurosc/kurosc/lib/gif_maker.py
+++ Python/kurosc/kurosc/lib/gif_maker.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 make gif from select folder
+credit to https://github.com/dm20/gif-maker
 """
 import imageio
 import os
 from os.path import isfile, join
 from pathlib import Path
 # from tkinter import filedialog
+
+
+"""
+https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
+fp_in = "/path/to/image_*.png"
+fp_out = "/path/to/image.gif"
+
+# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+img.save(fp=fp_out, format='GIF', append_images=imgs,
+         save_all=True, duration=200, loop=0)
+"""
+
 
 def CreateGif(targetpath:str = '.',
               ext:str = 'png',
