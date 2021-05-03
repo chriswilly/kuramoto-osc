fixed phase plot scaling to 0 to pi
added inferred titling based on %pi

Python/kurosc/kurosc/corticalSheet/oscillator.py
@@ -22,9 +22,11 @@ class oscillatorArray(object):
     def __init__(self,
                  m:int = 16,
                  n:int = 16,
+                 t:tuple = (0,np.pi),
                  output_level:int = 3  # not sure if required to be passing this thru
                  ):
         self.level = output_level
+        self.domain = t
         self.ic = self.initial_conditions(m,n)
         self.distance = self.complete_distance() # to follow IC
 
@@ -32,9 +34,10 @@ class oscillatorArray(object):
     def initial_conditions(self,
                            m:int = 16,
                            n:int = 16,
-                           scale: float = 2*np.pi,
-                           offset:float = -np.pi,
                            )->np.ndarray:
+        """return random phase array"""
+        scale = self.domain[1]
+        offset = self.domain[0]
         rng = np.random.default_rng()
         return scale*rng.random((m,n)) + offset
 
@@ -63,6 +66,9 @@ class oscillatorArray(object):
 
         return None
 
+
+
+
     def distance(self,
                  m:int = 16,
                  n:int = 16,
@@ -82,6 +88,7 @@ class oscillatorArray(object):
 
 
 
+
     def plot_phase(self,
                    X: np.ndarray,
                    plot_title:str = None,
@@ -93,9 +100,10 @@ class oscillatorArray(object):
         fig = plt.figure(figsize=(10,8))
         ax = fig.add_subplot(111)
 
-        scale = np.pi
         resolution = 16
-        colorscale = np.linspace(-scale,scale,resolution,endpoint=True)
+        colorscale = np.linspace(self.domain[0],
+                                 self.domain[1],
+                                 resolution,endpoint=True)
 
         plt.tricontourf(X[...,0],X[...,1],X[...,2],
                         colorscale,cmap=plt.cm.nipy_spectral,

Python/kurosc/kurosc/main.py
@@ -58,7 +58,8 @@ def build_ics(m:int = 128,
     """
     this demos how to construct a contour plot
     """
-    oscillators = oscillatorArray(m,n)
+    domain = (0,np.pi)
+    oscillators = oscillatorArray(m,n,domain)
     x = np.linspace(0,oscillators.ic.shape[0],
                       oscillators.ic.shape[1])
     y = np.linspace(0,oscillators.ic.shape[1],
@@ -69,9 +70,18 @@ def build_ics(m:int = 128,
                               y.flatten(),
                               oscillators.ic.flatten()]
                               ).T
-
+    if abs(domain[0]) % np.pi == 0 and not domain[0] == 0:
+        ti = r'\pi'
+        ti = '-'+ti
+    else:
+        ti = str(domain[0])
+    if abs(domain[1]) % np.pi == 0:
+        tf = r'\pi'
+    else:
+        tf = str(domain[1])
+        
     oscillators.plot_phase(phase_array,
-                           'Oscillator Phase $\in$ [-$\pi$,$\pi$)',
+                           'Oscillator Phase $\in$ [${0}$,${1}$)'.format(ti,tf),
                            'Location y',
                            'Location x'
                            )
@@ -122,8 +132,8 @@ def decouple():
 
 if __name__ == '__main__':
     build_ics(64,64)
-    spatial_kernel()
-    decouple()
+    # spatial_kernel()
+    # decouple()
 
 
     # print(Path(__file__).resolve())