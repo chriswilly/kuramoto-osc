fixed osc scaling
i forgot sin is only pi different from others

Python/kurosc/kurosc/corticalSheet/oscillator.py
@@ -36,8 +36,9 @@ class oscillatorArray(object):
                            n:int = 16,
                            )->np.ndarray:
         """return random phase array"""
-        scale = self.domain[1]
-        offset = self.domain[0]
+        scale = np.max(np.absolute(self.domain))
+        offset = np.min(self.domain)
+        # print(scale, offset)
         rng = np.random.default_rng()
         return scale*rng.random((m,n)) + offset
 
@@ -101,9 +102,11 @@ class oscillatorArray(object):
         ax = fig.add_subplot(111)
 
         resolution = 16
-        colorscale = np.linspace(self.domain[0],
-                                 self.domain[1],
-                                 resolution,endpoint=True)
+
+        colorscale = np.linspace(np.min(self.domain),
+                                 np.max(self.domain),
+                                 resolution,
+                                 endpoint=True)
 
         plt.tricontourf(X[...,0],X[...,1],X[...,2],
                         colorscale,cmap=plt.cm.nipy_spectral,

Python/kurosc/kurosc/main.py
@@ -52,6 +52,8 @@ from corticalSheet.oscillator import oscillatorArray
 from secondOrderInteraction.decouple import interaction
 from spatialKernel.wavelet import kernel
 
+np.set_printoptions(precision=3, suppress=True)
+
 
 def build_ics(m:int = 128,
               n:int = 128):
@@ -60,6 +62,7 @@ def build_ics(m:int = 128,
     """
     domain = (0,np.pi)
     oscillators = oscillatorArray(m,n,domain)
+
     x = np.linspace(0,oscillators.ic.shape[0],
                       oscillators.ic.shape[1])
     y = np.linspace(0,oscillators.ic.shape[1],
@@ -75,11 +78,11 @@ def build_ics(m:int = 128,
         ti = '-'+ti
     else:
         ti = str(domain[0])
-    if abs(domain[1]) % np.pi == 0:
+    if abs(domain[1]) % np.pi == 0 and not domain[1] == 0:
         tf = r'\pi'
     else:
         tf = str(domain[1])
-        
+
     oscillators.plot_phase(phase_array,
                            'Oscillator Phase $\in$ [${0}$,${1}$)'.format(ti,tf),
                            'Location y',
@@ -131,7 +134,7 @@ def decouple():
 
 
 if __name__ == '__main__':
-    build_ics(64,64)
+    build_ics(16,16)
     # spatial_kernel()
     # decouple()
 